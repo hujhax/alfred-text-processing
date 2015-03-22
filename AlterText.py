5218236068405b447232032446f21205602ab1af
Peter Rogers <axyaqehsmq@snkmail.com>
2015-03-22 16:38:44 -0500

    Adding hashtagger.

--- AlterText.py
+++ AlterText.py
@@ -3,6 +3,7 @@
 
 from ProcessSelection import setClipboardData, sendPasteKey, \
                              processSelectedString
+import argparse
 
 
 def hyphenateText(text):
@@ -10,4 +11,30 @@ def hyphenateText(text):
     sendPasteKey()
 
 
-processSelectedString(hyphenateText)
+def hashtagText(text):
+    textNoSpace = "".join(text.split())
+    hashtag = "#" + textNoSpace.lower()
+    setClipboardData(hashtag)
+    sendPasteKey()
+
+
+def parseArguments():
+    toolDescription = "A few functionalities for altering "\
+                      "the selected text."
+
+    parser = argparse.ArgumentParser(description=toolDescription)
+    behaviors = parser.add_mutually_exclusive_group()
+    behaviors.add_argument("--hyphenate", action="store_true",
+                           help="Replace spaces with hyphens.")
+    behaviors.add_argument("--hashtag", action="store_true",
+                           help="Convert the string to a hashtag, #likethis.")
+    return parser.parse_args()
+
+
+args = parseArguments()
+
+
+if args.hyphenate:
+    processSelectedString(hyphenateText)
+elif args.hashtag:
+    processSelectedString(hashtagText)
